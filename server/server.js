708d4565208fbf414fa66f8bc6da9316f228ff93
ABHIRAMANCH <151909943+ABHIRAMANCH@users.noreply.github.com>
2025-08-13 15:08:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://minijobb.netlify.app/",
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
